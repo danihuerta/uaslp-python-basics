09363a4bf2a851d4710966aa134d7f74f9cea359
Daniel Huerta <Daniel.Huerta.-ND@disney.com>
2023-07-04 11:03:37 -0500

    Init commit

---
+++ day_1_exercises/3_exercise.py
@@ -0,0 +1,2 @@
+# Escribir un programa que muestre por pantalla el resultado de la siguiente operación aritmética
+# 3 más 2, divido por la multiplicación de 2 por 5, el resultado elevado al cuadrado
