87dec8857037068f8ee71e36a7fb1ad606b3d0d2
Daniel Huerta <Daniel.Huerta.-ND@disney.com>
2023-07-11 17:31:24 -0600

    Day 1 resources

--- day_1_exercises/day_1.py
+++ day_1_exercises/day_1.py
@@ -1,38 +1,50 @@
-############## COMENTARIOS 
+############## PYTHON CLI ##############
+# Python tiene una CLI "Commands Line Interface" que te permite ejecutar comandos de Python en tiempo real
+# La forma en la que se accede a ella es escribiendo en la terminal 'python3' 
+# Para salir de ella, ejecuta exit()
+
+############## PYTHON EJECUTANDO ARCHIVOS ##############
+# Para que un archivo Python sea ejecutable, necesita que su extensión sea '.py'
+# Unicamente debes ejecutar en la terminal: python3 [Nombre del archivo].py
+
+############## COMENTARIOS ##############
+
+# Esto es un comentario de una sola línea
+
+# Los comentarios suelen usarse para describir qué hace X línea de código
+# Los comentarios suelen usarse también para evitar que cierto código se ejecute
 
 
-# Función para imprimir mensaje en pantalla
-# Test
-# Test
 """
-print("Hola Mundo!")
-print("Hola Mundo!")
-print("Hola Mundo!")
-print("Hola Mundo!")
-print("Hola Mundo!")
+Esto es un comentario
+de múltiples líneas :D
 """
 
-###################
-#### VARIABLES ####
+############## VARIABLES ##############
 
+'''
+Las variables no requieren que se les defina el tipo de dato
+que almacenarán, Python es capaz de distinguirlo y hacerlo por nosotros
+'''
 integer_var = 4 # ENTERO
-print(type(integer_var))
-
 float_var = 2.456 # FLOTANTE
-boolean_var = True # BOOL
-boolean_var = False # BOOL
+true_boolean_var = True # BOOL
+false_boolean_var = False # BOOL
 string_var = 'Hola' # STRING
+another_string_var = "Hola" # STRING
 
-integer_var = 3.1416
-print(type(integer_var))
+# Las variables pueden cambiar de tipo de dato a lo largo de la ejecución del programa
+integer_var = 5
+print(type(integer_var)) # IMPRIMIRÁ 'INT'
+integer_var = 5.5
+print(type(integer_var)) # AHORA IMPRIMIRÁ 'FLOAT'
 
-#####################
 
+# Técnicas para el nombramiento de variables de más de una palabra
 myVariableCamelCase = "Camel Case"
 MyPascalCamelCase = "Pascal Case"
 my_snake_variable = "Snake"
 
-#####################
 
 # MÚLTIPLES VALORES PARA VARIABLES
 x, y, z = "Orange", "Banana", "Cherry"
@@ -46,23 +58,46 @@ print(x)
 print(y)
 print(z)
 
-#####################
+############## OPERADORES ARTIMÉTICOS ##############
 
-# OPERADORES ARTIMÉTICOS
 num1 = 10 
 num2 = 35
+suma = num1+ num2
+resta = num1 - num2
+division = num1 / num2 # El resultado siempre será un FLOTANTE! 
+division_entera = num1 // num2 # El resultado siempre será el entero inferior más próximo
+multiplicacion = num1 * num2
 exponenciacion = num2 ** num1
-print(exponenciacion)
+
+print("Suma:", suma)
+print("Resta:", resta)
+print("División:",division)
+print("División entera",division_entera)
+print("Multiplicación:",multiplicacion)
+print("Exponenciación:",exponenciacion)
 
 
-######################
-print('##########################')
-# PRINT()
-# Caracter salto de línea: \n
-print('Hola', end='?')
-print('Hola','Mundo',sep='/')
+############## FUNCIÓN PRINT() ##############
+print('Hola Mundo')
+print('Hola','Mundo') # Por defecto, las palabras se separarán por un espacio
+print('Hola','Mundo',sep='/') # Puedo alterar ese comportamiento utilizando el argumento 'sep' y especificando el separador
+print('Hola', end='?') # Por defecto Python agrega un salto de línea, puedo alterar ese comportamiento con el argumento 'end'
 
-######################
-# INPUT()
+#### F-STRINGS ####
+# Te permiten formatear strings de una manera sencilla
+# Únicamente hay que colocar el string dentro de f''
+nombre = 'Daniel' # String
+edad   = 25 # Integer
+print(f'Mi nombre es {nombre} y tengo {edad} años')
+
+# Dentro de {...} puedes colocar variables o incluso operaciones como sumas, restas, llamadas a funciones, etc...
+# También puedes asignar a una variable tu f-string y después mandarla a llamar:
+my_f_string = f'Mi nombre es {nombre} y tengo {edad} años'
+print(my_f_string)
+
+
+############## FUNCIÓN INPUT() ##############
+# Sirve para pedir la entrada de datos al usuario, dentro de los paréntesis vas el mensaje que quieres mostrarle
+# La ejecución del programa se detiene, esperando el input del usuario
 username = input('Introduce tu username:')
 print(username)
